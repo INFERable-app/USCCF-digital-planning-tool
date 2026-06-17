79360fb559b3380cfa8a658109295d9c26024cb5
Natou <rnatou12@gmail.com>
2026-06-17 14:53:23 -0400

    Rename graph-editor to docs, update Pages source, expand challengeLabels

--- src/graph/wizardGraph.js
+++ src/graph/wizardGraph.js
@@ -1,11 +1,11 @@
-import graphData from '../../graph-editor/wizardGraph.json';
+import graphData from '../../docs/wizardGraph.json';
 
 export const startNodeId = 'welcome';
 
 export const challengeLabels = {
-    'ced-01': 'Establish/Expand network',
-    'ced-02': 'Convene employers',
-    'ced-03': 'Build consensus',
+    'ced-01': 'Establish/Expand employer collaborative network',
+    'ced-02': 'Convene employers to define shared hiring needs',
+    'ced-03': 'Build consensus on common job titles, roles, and requirements',
 };
 
 export const { nodes, edges } = graphData;
