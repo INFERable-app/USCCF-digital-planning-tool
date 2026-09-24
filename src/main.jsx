61ff36dd94a2edb27c3224cc5d2d0a452e0cd5c4
Natou <rnatou12@gmail.com>
2026-09-24 09:59:27 -0400

    Add USCCF color theme tokens

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { AuthProvider } from './contexts/AuthContext.jsx';
+import './theme.css';
 import './App.css';
 import App from './App.jsx';
 
